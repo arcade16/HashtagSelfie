f00bf6511a32271b41a3a2bb0164fc623ce69dfb
arcade16 <PR3DAT0R@MobileStrikePlatform.local>
2014-12-14 17:05:04 -0500

    instagram api proof of concept

---
+++ app/src/main/java/com/creeperware/roumen/hashtagselfie/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.creeperware.roumen.hashtagselfie;
+
+import java.lang.String;public class ApplicationData {
+	public static final String CLIENT_ID = "86d5775df29640379889bfa913fc1bcb";
+	public static final String CLIENT_SECRET = "061fca08b5c94e90a3b5c97eef288ac8";
+	public static final String CALLBACK_URL = "http://www.linkedin.com/pub/roumen-kassabov/74/5aa/52a/";
+}
